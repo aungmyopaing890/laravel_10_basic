d41f9751b115c03e7e25dc4e17348fa5b001c79e
aungmyopaing890 <aungmyopaing890@gmail.com>
2023-05-17 22:55:06 +0630

    for all middleware adding in kernal  middleware and logging route requests

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use App\Http\Controllers\ItemApiController;
-use App\Http\Middleware\CheckApiToken;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,9 +8,6 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
 |
 */
 
@@ -21,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::apiResource("item", ItemApiController::class)->middleware(CheckApiToken::class);
+Route::apiResource("item", ItemApiController::class)->middleware('cat');
